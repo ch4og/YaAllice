5eab294dba0091156ac28787662f34863cd324f8
Leb Nickley <egorcraft@gmail.com>
2018-10-20 21:55:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const Alice  = require('yandex-dialogs-sdk')
 const alice = new Alice()
 
+alice.command('Что происходит', ctx => {
+	if(users.includes(ctx.userId)){
+		return ctx.reply('О, я тебя помню!')
+	}
+	
+	users.push(ctx.userId)
+	ctx.reply('Привет! Я умею считать площадь треугольника. Ну, если честно, то должна немного больше уметь, но что умею, то умею... Кстати, можешь спросить у меня помощи.')
+})
 
 alice.any(ctx => {
   	ctx.reply('Приветиk')
