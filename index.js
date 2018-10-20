90694bb099a0cc5915b7304e889c9cd434a9e77b
4u3 <32384814+4u3@users.noreply.github.com>
2018-10-20 22:08:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const Alice  = require('yandex-dialogs-sdk')
 const alice = new Alice()
 //AAA
 //
-const users = [];
+//const users = [];
 
 alice.command('Что происходит', ctx => {
-	if(users.includes(ctx.userId)){
-		return ctx.reply('О, я тебя помню!')
-	}
+	//if(users.includes(ctx.userId)){
+	//	return ctx.reply('О, я тебя помню!')
+	//}
 	
 	users.push(ctx.userId)
 	ctx.reply('Привет! Я умею считать площадь треугольника. Ну, если честно, то должна немного больше уметь, но что умею, то умею... Кстати, можешь спросить у меня помощи.')
